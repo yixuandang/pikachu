c9928a9cae078a0e5859cf9abd9341a3b246a004
Yixuan D ang <ydang@e1z2r8p27.42.us.org>
2016-11-19 18:07:58 -0800

    pikachu.h file

---
+++ includes/pikachu.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pikachu.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/18 14:05:12 by ydang             #+#    #+#             */
+/*   Updated: 2016/11/18 14:06:57 by ydang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIKACHU_H
+# define PIKACHU_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "./libft/includes/libft.h"
+
+void	test_color(void);
+
+#endif
